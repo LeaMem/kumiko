58908c4ebf962642dd0e0dc624e119100624425c
Kumiko <944506900@qq.com>
2020-07-08 23:21:16 +0800

    784

---
+++ leet/src/main/java/com/lea/leet/alg/Solution200.java
@@ -0,0 +1,52 @@
+package com.lea.leet.alg;
+
+public class Solution200 {
+
+    public int numIslands(char[][] grid) {
+
+        int cnt = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    cnt++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+
+    public void dfs(char[][] grid, int i, int j) {
+
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0';
+
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+
+    }
+
+    public static void main(String[] args) {
+
+        char[][] grid = {
+                {'1', '1', '0', '0', '0'},
+                {'1', '1', '0', '0', '0'},
+                {'0', '0', '1', '0', '0'},
+                {'0', '0', '0', '1', '1'}
+        };
+
+        Solution200 solution200 = new Solution200();
+        int ans = solution200.numIslands(grid);
+        System.out.println(ans);
+
+    }
+
+
+}
--- leet/src/main/java/com/lea/leet/alg/Solution784.java
+++ leet/src/main/java/com/lea/leet/alg/Solution784.java
@@ -9,15 +9,29 @@ public class Solution784 {
 
         List<String> list = new ArrayList<>();
 
-        dfs(list, new StringBuilder(S), 0);
+        dfs(list, "", S, 0);
 
         return list;
 
     }
 
-    public void dfs(List<String> list, StringBuilder builder, int index){
+    public void dfs(List<String> list, String str, String s, int index) {
+
+        if (index == s.length()) {
+            list.add(str);
+            return;
+        }
 
 
+        int f = index;
+        int k = index;
+        dfs(list, str + s.charAt(index), s, ++f);
+
+        if (Character.isUpperCase(s.charAt(index))) {
+            dfs(list, str + Character.toLowerCase(s.charAt(index)), s, ++k);
+        } else if (Character.isLowerCase(s.charAt(index))) {
+            dfs(list, str + Character.toUpperCase(s.charAt(index)), s, ++k);
+        }
 
     }
 
@@ -27,7 +41,7 @@ public class Solution784 {
         Solution784 solution784 = new Solution784();
         List<String> list = solution784.letterCasePermutation(str);
 
-        for(String it : list){
+        for (String it : list) {
             System.out.println(it);
         }
 
