4bbdf824bee22b30cd208ac72078f12c5d24b509
cuijin <cuijin03@changingedu.com>
2020-08-10 19:25:32 +0800

    655终于好了，我竟然忘记了dfs的写法，

--- leet/src/main/java/com/lea/leet/alg/Solution655.java
+++ leet/src/main/java/com/lea/leet/alg/Solution655.java
@@ -21,23 +21,22 @@ public class Solution655 {
             list.add(tmpList);
         }
 
-        dfs(root, 0, size / 2, list);
+        dfs(list, 0, 0, size - 1, root);
         return list;
     }
 
-    public void dfs(TreeNode root, int height, int index, List<List<String>> list) {
+    public void dfs(List<List<String>> list, int height, int l, int r, TreeNode root) {
         if (root == null) {
             return;
         }
+        int index = (l + r) / 2;
         List<String> tmpList = list.get(height);
         tmpList.set(index, "" + root.val);
         height++;
-        int size = (int) Math.pow(2, height) - 1;
-        int left = (index - 1) / 2;
-        int right = size - left;
-        dfs(root.left, height, left, list);
-        dfs(root.right, height, right, list);
+        dfs(list, height, l, index, root.left);
+        dfs(list, height, index + 1, r, root.right);
     }
+
 
     public int getHeight(TreeNode root) {
         if (root == null) {
@@ -51,10 +50,12 @@ public class Solution655 {
     public static void main(String[] args) {
 
         Solution655 solution655 = new Solution655();
-        TreeNode root = new TreeNode(1);
-        root.left = new TreeNode(2);
-        root.right = new TreeNode(3);
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
         root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(7);
 
         List<List<String>> lists = solution655.printTree(root);
         for (List<String> list : lists) {
