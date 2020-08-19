f824d86da1947ed2112181829262e3b7d6689d20
cuijin <cuijin03@changingedu.com>
2020-08-20 00:24:17 +0800

    1530终于冲好了

--- leet/src/main/java/com/lea/leet/alg/Solution1530.java
+++ leet/src/main/java/com/lea/leet/alg/Solution1530.java
@@ -3,25 +3,69 @@ package com.lea.leet.alg;
 import com.lea.leet.alg.base.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution1530 {
 
-    List<Integer> list = new ArrayList<>();
-
+    int dis;
+    int cnt;
     public int countPairs(TreeNode root, int distance) {
-        return 0;
+        this.dis = distance;
+        this.cnt = 0;
+        dfs(root);
+        return this.cnt;
     }
 
-    public void dfs(TreeNode root, int x){
+    public List<Integer> dfs(TreeNode root){
+
         if(root == null){
-            return ;
+            return new ArrayList<>();
         }
+
         if(root.left == null && root.right == null){
-            list.add(x);
+            return Collections.singletonList(1);
         }
-        dfs(root.left, 2 * x + 1);
-        dfs(root.right, 2 * x + 2);
+
+        List<Integer> left = dfs(root.left);
+        List<Integer> right = dfs(root.right);
+
+        for(Integer l : left){
+            for (Integer r : right){
+                if(l + r <= dis){
+                    cnt++;
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(Integer it : left){
+            int tmp = it + 1;
+            if(tmp <= dis){
+                list.add(tmp);
+            }
+        }
+
+        for(Integer it : right){
+            int tmp = it + 1;
+            if(tmp <= dis){
+                list.add(tmp);
+            }
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+
+        Solution1530 solution1530 = new Solution1530();
+        int ans = solution1530.countPairs(root, 3);
+        System.out.println(ans);
     }
 
 }
